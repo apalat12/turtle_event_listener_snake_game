fe0bb556dd7a53afb5863b31c04246cd35d37e2c
Abhijeet Palatkar <apalatkar@marvell.com>
2021-05-15 18:28:18 -0700

    bug fix in food

--- food_new.py
+++ food_new.py
@@ -1,16 +1,16 @@
 from turtle import Turtle
-<<<<<<< HEAD
+
 from random import randint
-=======
+
 import random
->>>>>>> bf14fe4e2baf23a780953c07aa79f66a7ba33bfb
+
 
 
 class Food(Turtle):
 
     def __init__(self):
         super().__init__()
-<<<<<<< HEAD
+
         self.shape('circle')
         self.penup()
         self.shapesize(0.5, 0.5)
@@ -18,9 +18,9 @@ class Food(Turtle):
         self.speed('fastest')
         self.goto(randint(-280, 280), randint(-280, 280))
 
-    def refresh(self):
+    def reset(self):
         return self.__init__()
-=======
+
         self.shape("circle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
@@ -32,4 +32,4 @@ class Food(Turtle):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
->>>>>>> bf14fe4e2baf23a780953c07aa79f66a7ba33bfb
+
